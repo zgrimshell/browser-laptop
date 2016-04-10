5e9b1ca24c4e0874ec29953e4249203e11a989e3
yan <yan@mit.edu>
2016-04-09 20:44:54 -0700

    Ensure that passwords' form action is a string
    Fix #1305
    
    Auditors: @bbondy

--- app/content/scripts/webviewPreload.js
+++ app/content/scripts/webviewPreload.js
@@ -240,6 +240,9 @@
    * @return {string}
    */
   function normalizeURL (url) {
+    if (typeof url !== 'string') {
+      return ''
+    }
     var a = document.createElement('a')
     a.href = url
     return [a.protocol, a.host].join('//') + a.pathname
